a47ff114bbae667b7f7f04af86d6ef49d64b00ec
philipretl <philipretl@gmail.com>
2022-04-01 15:28:02 -0500

    feature: Validation of required fields ok

--- apps/backend/app/Http/Controllers/Api/UsersController.php
+++ apps/backend/app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Contracts\UsersService;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveUserRequest;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Venoudev\Results\ApiJsonResources\PaginatedResource;
@@ -43,67 +44,12 @@ class UsersController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param SaveUserRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function store(SaveUserRequest $request)
     {
         //
     }
